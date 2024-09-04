fab36e2cd174a9cbaabaec1546c640de3ba03654
rimasultana <rimasultana20225@gmail.com>
2024-09-04 18:20:08 +0600

    reverse

--- methods.js
+++ methods.js
@@ -26,10 +26,68 @@
 // console.log(fruits);
 
 // Array shift():
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // fruits.shift();
 // console.log(fruits);
 
 // unshift():
-fruits.unshift('lemon');
-console.log(fruits);
+// fruits.unshift('lemon');
+// console.log(fruits);
+
+//changing Element
+
+// fruits[1] ='Siwi';
+// console.log(fruits);
+
+// Js Array length
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits[fruits.length] = 'Kiwi';
+// console.log(fruits);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// delete fruits[2];
+// console.log(fruits);
+
+// Concatenating Array
+
+// const myGrils = ["Cecilie", "Lone"];
+// const myBoys = ["Email", "tobias", "Linus"];
+// const myChildren = myGrils.concat(myBoys);
+// console.log(myChildren);
+
+// Array copyWithin():
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const fruit = fruits.copyWithin(2,3);
+
+// console.log(fruit);
+
+// 
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2,3,'lemon', 'lichu');
+// console.log(fruits);
+// Array slice():
+// const citius = fruits.slice(2);
+// console.log(citius);
+
+// search Array
+
+// const fruits = ['Apple', 'Orange', 'Apple', 'Mango'];
+// let position =fruits.indexOf("Apple") + 1;
+// console.log(position);
+// let position =fruits.lastIndexOf('Apple') + 1;
+// console.log(position);
+// let position =fruits.indexOf('Orange') + 1;
+// console.log(position);
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const x =  fruits.includes("Mango");
+// console.log(x);
+
+
+// Array sort():
+const fruit =['Banana', 'Orange', 'Apple', 'Mango'];
+fruit.sort();
+console.log(fruit);
+fruit.reverse();
+console.log(fruit);
